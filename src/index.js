3d25656dbd0c99f8f38a1259848a2407e65ab442
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-08-11 09:54:08 -0700

    Achieve Editing In The UI

--- src/index.js
+++ src/index.js
@@ -23,5 +23,20 @@ editTheList.forEach((item) => {
     e.target.parentElement.classList.remove('sibling');
     e.target.parentElement.classList.add('none');
     e.target.parentElement.nextElementSibling.classList.remove('none');
+    e.target.parentElement.nextElementSibling.style.display = 'flex';
+    const form = e.target.parentElement.nextElementSibling;
+    form.style.justifyContent = 'space-around';
+    form.style.alignItems = 'center';
+    form.style.width = '100%';
+    e.target.parentElement.parentElement.children[3].classList.add('none');
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const theValue = e.target.children[0].value;
+      e.target.parentElement.children[0].classList.remove('none');
+      e.target.parentElement.children[0].children[1].textContent = theValue;
+      e.target.parentElement.classList.add('sibling');
+      e.target.parentElement.children[3].classList.remove('none');
+      e.target.style.display = 'none';
+    });
   });
 });
